6f6cbfebb6e49e0fb5cae0aed7cd105c70da7c08
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-08-20 10:40:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// // code your solution here
+function saturdayFun( whatDoing= "roller-skate"){
+    return (`This Saturday, I want to ${whatDoing}!`);
+    
+}
+
+
+function mondayWork(MondayActivity = "go to the office"){
+    return (`This Monday, I will ${MondayActivity}.`)
+
+}
+
+
+function wrapAdjective(initialized = "*"){
+    return function(pipeInitial = "special"){
+        return `You are ${initialized}${pipeInitial}${initialized}!`;
+    }
+}
+
+
+  
